20870fd730ad85747ebd643820a8d8e880c6f953
Eduardo Echenagucia <eduardonyx.ee@gmail.com>
2024-11-27 16:50:27 -0300

    version inicial

---
+++ src/main/java/com/echenagucia/screenmatchfrases/screenmacthfrases/ScreenmacthfrasesApplication.java
@@ -0,0 +1,13 @@
+package com.echenagucia.screenmatchfrases.screenmacthfrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmacthfrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmacthfrasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/echenagucia/screenmatchfrases/screenmacthfrases/ScreenmacthfrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echenagucia.screenmatchfrases.screenmacthfrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmacthfrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
